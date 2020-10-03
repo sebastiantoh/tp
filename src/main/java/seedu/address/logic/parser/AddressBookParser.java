ca6668f84909c9d8b9acac84cda31735758aa6e2
Aaron S <aaronsgseah@gmail.comm>
2020-10-03 22:42:43 +0800

    Remove unused variable

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -24,8 +24,6 @@ public class AddressBookParser {
      */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
 
-    private static final String CONTACT_COMMAND = "contact";
-
     /**
      * Parses user input into command for execution.
      *
