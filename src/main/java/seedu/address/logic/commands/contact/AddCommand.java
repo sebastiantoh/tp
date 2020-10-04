22adc16411a8e83d23e9c9374bd1f613c5d84e76
Kwek Min Yih <minyihkwek@gmail.com>
2020-10-04 19:53:02 +0800

    Rename Prefix constants in CliSyntax

--- src/main/java/seedu/address/logic/commands/contact/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/contact/AddCommand.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands.contact;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTACT_TAG;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
@@ -21,18 +21,18 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: "
-            + PREFIX_NAME + "NAME "
-            + PREFIX_PHONE + "PHONE "
-            + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_TAG + "TAG]...\n"
+            + PREFIX_CONTACT_NAME + "NAME "
+            + PREFIX_CONTACT_PHONE + "PHONE "
+            + PREFIX_CONTACT_EMAIL + "EMAIL "
+            + PREFIX_CONTACT_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_CONTACT_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_CONTACT_NAME + "John Doe "
+            + PREFIX_CONTACT_PHONE + "98765432 "
+            + PREFIX_CONTACT_EMAIL + "johnd@example.com "
+            + PREFIX_CONTACT_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_CONTACT_TAG + "friends "
+            + PREFIX_CONTACT_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -5,11 +5,11 @@ package seedu.address.logic.parser;
  */
 public class CliSyntax {
 
-    /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    /* Prefix definitions for contacts*/
+    public static final Prefix PREFIX_CONTACT_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_CONTACT_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_CONTACT_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_CONTACT_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_CONTACT_TAG = new Prefix("t/");
 
 }
