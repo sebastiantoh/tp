a4a958977c1383af7df4529d2a783ff92d2c3c88
sebastiantoh <sebas.tsj.98@gmail.com>
2020-10-04 21:06:03 +0800

    Add UniqueReminderListTest

---
+++ src/test/java/seedu/address/model/reminder/UniqueReminderListTest.java
@@ -0,0 +1,109 @@
+package seedu.address.model.reminder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalReminders.CALL_ALICE;
+import static seedu.address.testutil.TypicalReminders.EMAIL_BOB;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.reminder.exceptions.DuplicateReminderException;
+import seedu.address.model.reminder.exceptions.ReminderNotFoundException;
+
+public class UniqueReminderListTest {
+    private final UniqueReminderList uniqueReminderList = new UniqueReminderList();
+
+    @Test
+    public void contains_nullReminder_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueReminderList.contains(null));
+    }
+
+    @Test
+    public void contains_reminderNotInList_returnsFalse() {
+        assertFalse(uniqueReminderList.contains(CALL_ALICE));
+    }
+
+    @Test
+    public void contains_reminderInList_returnsTrue() {
+        uniqueReminderList.add(CALL_ALICE);
+        assertTrue(uniqueReminderList.contains(CALL_ALICE));
+    }
+
+    @Test
+    public void add_nullReminder_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueReminderList.add(null));
+    }
+
+    @Test
+    public void add_duplicateReminder_throwsDuplicateReminderxception() {
+        uniqueReminderList.add(CALL_ALICE);
+        assertThrows(DuplicateReminderException.class, () -> uniqueReminderList.add(CALL_ALICE));
+    }
+
+    @Test
+    public void remove_nullReminder_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueReminderList.remove(null));
+    }
+
+    @Test
+    public void remove_reminderDoesNotExist_throwsReminderNotFoundException() {
+        assertThrows(ReminderNotFoundException.class, () -> uniqueReminderList.remove(CALL_ALICE));
+    }
+
+    @Test
+    public void remove_existingReminder_removesReminder() {
+        uniqueReminderList.add(CALL_ALICE);
+        uniqueReminderList.remove(CALL_ALICE);
+        UniqueReminderList expectedUniqueReminderList = new UniqueReminderList();
+        assertEquals(expectedUniqueReminderList, uniqueReminderList);
+    }
+
+    @Test
+    public void setReminders_nullUniqueReminderList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueReminderList.setReminders((UniqueReminderList) null));
+    }
+
+    @Test
+    public void setReminders_uniqueReminderList_replacesOwnListWithProvidedUniqueReminderList() {
+        uniqueReminderList.add(CALL_ALICE);
+        UniqueReminderList expectedUniqueReminderList = new UniqueReminderList();
+        expectedUniqueReminderList.add(EMAIL_BOB);
+        uniqueReminderList.setReminders(expectedUniqueReminderList);
+        assertEquals(expectedUniqueReminderList, uniqueReminderList);
+    }
+
+    @Test
+    public void setReminders_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueReminderList.setReminders((List<Reminder>) null));
+    }
+
+    @Test
+    public void setReminders_list_replacesOwnListWithProvidedList() {
+        uniqueReminderList.add(CALL_ALICE);
+        List<Reminder> reminderList = Collections.singletonList(EMAIL_BOB);
+        uniqueReminderList.setReminders(reminderList);
+        UniqueReminderList expectedUniqueReminderList = new UniqueReminderList();
+        expectedUniqueReminderList.add(EMAIL_BOB);
+        assertEquals(expectedUniqueReminderList, uniqueReminderList);
+    }
+
+    @Test
+    public void setReminders_listWithDuplicateReminders_throwsDuplicateReminderException() {
+        List<Reminder> listWithDuplicateReminders = Arrays.asList(CALL_ALICE, CALL_ALICE);
+        assertThrows(DuplicateReminderException.class, ()
+            -> uniqueReminderList.setReminders(listWithDuplicateReminders));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+            -> uniqueReminderList.asUnmodifiableObservableList().remove(0));
+    }
+
+}
