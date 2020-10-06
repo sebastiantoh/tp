3e1836fed1c1094d595f2f6a1e3513c1ab080c6b
sebastiantoh <sebas.tsj.98@gmail.com>
2020-10-06 17:40:12 +0800

    Add tests for appointments in JsonSeralizableAddressBookTest

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -20,10 +20,19 @@ public class JsonSerializableAddressBookTest {
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
 
+    private static final Path INVALID_PERSON_APPOINTMENT_FILE =
+        TEST_DATA_FOLDER.resolve("invalidPersonInAppointmentAddressBook.json");
+    private static final Path INVALID_DURATION_APPOINTMENT_FILE =
+        TEST_DATA_FOLDER.resolve("invalidDurationInAppointmentAddressBook.json");
+    private static final Path INVALID_DATE_APPOINTMENT_FILE =
+        TEST_DATA_FOLDER.resolve("invalidDateInAppointmentAddressBook.json");
+    private static final Path DUPLICATE_APPOINTMENT_FILE = TEST_DATA_FOLDER.resolve("duplicateAppointmentsAddressBook"
+        + ".json");
+
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
-                JsonSerializableAddressBook.class).get();
+            JsonSerializableAddressBook.class).get();
         AddressBook addressBookFromFile = dataFromFile.toModelType();
         AddressBook typicalPersonsAddressBook = TypicalPersons.getTypicalAddressBook();
         assertEquals(addressBookFromFile, typicalPersonsAddressBook);
@@ -39,9 +48,37 @@ public class JsonSerializableAddressBookTest {
     @Test
     public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
+            JsonSerializableAddressBook.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON,
-                dataFromFile::toModelType);
+            dataFromFile::toModelType);
     }
 
+    @Test
+    public void toModelType_invalidPersonInAppointment_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_APPOINTMENT_FILE,
+            JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDurationInAppointment_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_DURATION_APPOINTMENT_FILE,
+            JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDateInAppointment_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_DATE_APPOINTMENT_FILE,
+            JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_duplicateAppointments_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_APPOINTMENT_FILE,
+            JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_APPOINTMENT,
+            dataFromFile::toModelType);
+    }
 }
