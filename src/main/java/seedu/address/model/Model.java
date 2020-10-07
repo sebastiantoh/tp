78f6f11200505a6cc25f56a49697b979147f3d0c
sebastiantoh <sebas.tsj.98@gmail.com>
2020-10-07 08:08:17 +0800

    Add fullstop to javadoc

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -79,7 +79,7 @@ public interface Model {
     void setPerson(Person target, Person editedPerson);
 
     /**
-     * Returns an unmodifiable view of the filtered person list
+     * Returns an unmodifiable view of the filtered person list.
      */
     ObservableList<Person> getFilteredPersonList();
 
