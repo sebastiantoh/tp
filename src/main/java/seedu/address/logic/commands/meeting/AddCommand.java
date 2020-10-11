9638c6e0fd24e0f0b3b835d26779e9f5acc8bddb
sebastiantoh <sebas.tsj.98@gmail.com>
2020-10-11 09:36:43 +0800

    Use getSortedPersonList instead

--- src/main/java/seedu/address/logic/commands/meeting/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/meeting/AddCommand.java
@@ -70,7 +70,7 @@ public class AddCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        List<Person> lastShownList = model.getFilteredPersonList();
+        List<Person> lastShownList = model.getSortedPersonList();
 
         if (index.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
