9eb0606a6a75af115d2deda76662cfe0792e4e20
sebastiantoh <sebas.tsj.98@gmail.com>
2020-10-15 10:03:49 +0800

    Add MeetingCard component

---
+++ src/main/java/seedu/address/ui/MeetingCard.java
@@ -0,0 +1,61 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.meeting.Meeting;
+
+/**
+ * An UI component that displays information of a {@code Meeting}.
+ */
+public class MeetingCard extends UiPart<Region> {
+
+    private static final String FXML = "MeetingListCard.fxml";
+
+    public final Meeting meeting;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label id;
+    @FXML
+    private Label message;
+    @FXML
+    private Label startDate;
+    @FXML
+    private Label endDate;
+    @FXML
+    private Label personName;
+
+    /**
+     * Creates a {@code MeetingCard} with the given {@code Meeting} and index to display.
+     */
+    public MeetingCard(Meeting meeting, int displayedIndex) {
+        super(FXML);
+        this.meeting = meeting;
+        id.setText(displayedIndex + ". ");
+        message.setText(meeting.getMessage());
+        startDate.setText(meeting.getFormattedStartDate());
+        endDate.setText(meeting.getFormattedEndDate());
+        personName.setText(meeting.getPerson().getName().fullName);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof MeetingCard)) {
+            return false;
+        }
+
+        // state check
+        MeetingCard card = (MeetingCard) other;
+        return id.getText().equals(card.id.getText())
+                && meeting.equals(card.meeting);
+    }
+}
