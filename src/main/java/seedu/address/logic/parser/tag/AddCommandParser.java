e561e685e564b098e6d381772779104cf56c5626
Asthenosphere <e0271247@u.nus.edu>
2020-10-21 19:31:18 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/tag/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/tag/AddCommandParser.java
@@ -12,7 +12,6 @@ import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.tag.Tag;
 
 public class AddCommandParser implements Parser<AddCommand> {
 
