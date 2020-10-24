bffa5c4532e20d78141a441d4c28cd2fc5370160
Kwek Min Yih <minyihkwek@gmail.com>
2020-10-24 20:52:15 +0800

    Add tests to DeleteCommandParserTest for Sales

--- src/test/java/seedu/address/logic/parser/sale/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/sale/DeleteCommandParserTest.java
@@ -5,23 +5,34 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_SALE_INDEX;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ITEM;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_ITEM;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_ITEM;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.sale.DeleteCommand;
 
 public class DeleteCommandParserTest {
     private DeleteCommandParser parser = new DeleteCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
+    public void parse_validSingleIndex_returnsDeleteCommand() {
         assertParseSuccess(parser, " " + PREFIX_SALE_INDEX + "1",
                 new DeleteCommand(new ArrayList<>(Arrays.asList(INDEX_FIRST_ITEM))));
+    }
+
+    @Test
+    public void parse_validMultipleIndexes_returnsDeleteCommand() {
+        assertParseSuccess(parser, " " + PREFIX_SALE_INDEX + "1 " + PREFIX_SALE_INDEX + "3",
+                new DeleteCommand(new ArrayList<>(Arrays.asList(INDEX_FIRST_ITEM, INDEX_THIRD_ITEM))));
+    }
+
+    @Test
+    public void parse_invalidMultipleIndexes_returnsDeleteCommand() {
+        assertParseFailure(parser, " " + PREFIX_SALE_INDEX + "1 " + PREFIX_SALE_INDEX + "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 
     @Test
