a94599ca1589f55cd6ed3b2ec8bd68e555dfba36
Kwek Min Yih <minyihkwek@gmail.com>
2020-10-27 15:09:02 +0800

    Fix tests

--- src/test/java/seedu/address/model/ModelStub.java
+++ src/test/java/seedu/address/model/ModelStub.java
@@ -5,6 +5,7 @@ import java.time.Month;
 import java.time.Year;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
@@ -132,7 +133,7 @@ public class ModelStub implements Model {
     }
 
     @Override
-    public boolean saleTagsExist(Sale sale) {
+    public boolean saleTagsExist(Set<Tag> tag) {
         return true;
     }
 
